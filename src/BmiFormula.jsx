d5a0774292bb23c27d94992fba02b7649273b63a
Muneera <muneerakottarath99@gmail.com>
2024-09-28 14:29:13 +0530

    small change

--- src/BmiFormula.jsx
+++ src/BmiFormula.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function BmiFormula() {
   return (
     <div className='w-fit h-fit flex gap-4 items-center'>
-        BMI = <div className='flex flex-col'><h1>Weight(Kg)</h1>
+        BMI = <div className='flex flex-col'><h1>Weight(kg)</h1>
                                              <hr></hr>
                                              <h1>Height(m) * Height(m)</h1>
               </div>
